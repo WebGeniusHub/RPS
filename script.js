053a8ca0205e202aa245824c13ac7fa7ca0d346e
Erisan Rakipi <ersian813@gmail.com>
2023-06-14 16:23:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const gameContainer = document.querySelector('.container'),
+userResult = document.querySelector('.user_result img'),
+cpuResult = document.querySelector('.cpu_result img'),
+result = document.querySelector('.result'),
+optionImage = document.querySelectorAll('.option_image');
+
+optionImage.forEach((image, index) =>{
+    image.addEventListener('click', (e) =>{
+        image.classList.add('active');
+
+
+        optionImage.forEach((image2, index2) => {
+            index !== index2 && image2.classList.remove('active');
+
+        });
+
+        gameContainer.classList.add('start');
+
+        let time = setTimeout (() =>{
+            gameContainer.classList.remove('start');
+            let imageSrc = e.target.querySelector('img').src;
+        userResult.src = imageSrc;
+
+        let randomNumber = Math.floor(Math.random () * 3)
+        
+        let cpuImages = ['images/rock.png','images/paper.png', 'images/scissors.png']
+        cpuResult.src = cpuImages[randomNumber]
+
+        let cpuValue = ['R', 'P', 'S'][randomNumber]
+        let userValue = ['R', 'P', 'S'][index]
+
+        console.log(cpuValue,userValue)
+
+
+        let outcome = {
+            RR: 'Drow',
+            RP: 'Computer',
+            RS: 'User',
+            RP: 'Computer',
+            PP: 'Drow',
+            PR: 'Computer',
+            PS: 'User',
+            SS: 'Drow',
+            SR: 'Computer',
+            SP: 'User',
+
+        };
+
+        let outComeValue = outcome[userValue + cpuValue]
+
+        result.textContent = userValue === cpuValue ? 'Match Draw' : `${outComeValue} Won !!`
+        },2500)
+        
+    })
+})
